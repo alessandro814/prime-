8c179c2a7a4cd7fe33475163a8817b782fbe91f0
alessandro814 <alessandro@blackforestsupplements.com>
2026-05-26 07:33:49 -0400

    Actualizar sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,17 @@
-const CACHE_NAME='prime-v1';
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(['./index.html'])));self.skipWaiting();});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
+const CACHE = 'prime-v3';
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => caches.delete(key)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
